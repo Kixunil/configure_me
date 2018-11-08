66625a36fdabb0ca81138e011408b9e633f052e2
Martin Habovstiak <martin.habovstiak@gmail.com>
2018-11-08 11:05:40 +0100

    Added tests of remaining args.
    It's now tested whether remaining arguments are correctly returned and
    whether two dashes correctly cut them off.

--- configure_me_codegen/tests/custom_args.rs
+++ configure_me_codegen/tests/custom_args.rs
@@ -2,10 +2,38 @@ macro_rules! test_name { () => { "single_optional_param" } }
 
 include!("glue/boilerplate.rs");
 
-#[test]
-fn main() {
-    use std::iter;
-    use std::path::PathBuf;
+use std::iter;
+use std::path::PathBuf;
 
-    let _ = config::Config::custom_args_and_optional_files(&["custom_args", "--foo", "42"], iter::empty::<PathBuf>()).unwrap();
+#[test]
+fn custom_args() {
+    let (config, mut remaining) = config::Config::custom_args_and_optional_files(&["custom_args", "--foo", "42"], iter::empty::<PathBuf>()).unwrap();
+    assert_eq!(config.foo, Some(42));
+    assert_eq!(remaining.next(), None);
+}
+
+#[test]
+fn custom_args_with_one_remaining() {
+    let (config, mut remaining) = config::Config::custom_args_and_optional_files(&["custom_args", "--foo", "42", "remaining_arg"], iter::empty::<PathBuf>()).unwrap();
+    assert_eq!(config.foo, Some(42));
+    assert_eq!(remaining.next(), Some("remaining_arg".into()));
+    assert_eq!(remaining.next(), None);
+}
+
+#[test]
+fn custom_args_with_two_remaining() {
+    let (config, mut remaining) = config::Config::custom_args_and_optional_files(&["custom_args", "--foo", "42", "remaining_arg", "another_arg"], iter::empty::<PathBuf>()).unwrap();
+    assert_eq!(config.foo, Some(42));
+    assert_eq!(remaining.next(), Some("remaining_arg".into()));
+    assert_eq!(remaining.next(), Some("another_arg".into()));
+    assert_eq!(remaining.next(), None);
+}
+
+#[test]
+fn custom_args_with_two_dashes() {
+    let (config, mut remaining) = config::Config::custom_args_and_optional_files(&["custom_args", "--", "--foo", "42"], iter::empty::<PathBuf>()).unwrap();
+    assert_eq!(config.foo, None);
+    assert_eq!(remaining.next(), Some("--foo".into()));
+    assert_eq!(remaining.next(), Some("42".into()));
+    assert_eq!(remaining.next(), None);
 }
